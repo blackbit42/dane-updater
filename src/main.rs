e5da85215500a8523ed02f4bb37cc83cc5435f1a
Alexander Huemer <alexander.huemer@xx.vu>
2024-08-30 15:22:32 +0200

    fix clippy warning

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ impl Pubkeys {
 
 pub fn sha256(data: &[u8]) -> Vec<u8> {
     let mut hasher = Sha256::new();
-    hasher.update(&data);
+    hasher.update(data);
     hasher.finalize().to_vec()
 }
 
