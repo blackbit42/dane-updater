b38bad92918943a5b79b174b54e4a0f460326cd0
Alexander Huemer <alexander.huemer@xx.vu>
2024-09-06 21:01:43 +0200

    initial dynamic DNS update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod pubkey;
+pub mod tsig;
 
 pub use pubkey::load_pubkey;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+use anyhow::Context as _;
 use clap::Parser;
-use dane_updater::load_pubkey;
-use hickory_client::rr::{
-    rdata::{tlsa, TLSA},
-    RData,
+use dane_updater::{load_pubkey, tsig};
+use data_encoding::BASE64;
+use hickory_client::{
+    client::{Client, SyncClient},
+    rr::{
+        rdata::{tlsa, tsig::TsigAlgorithm, TLSA},
+        Name, RData, Record, RecordType,
+    },
+    tcp::TcpClientConnection,
 };
+use hickory_proto::rr::dnssec::tsig::TSigner;
 use hickory_resolver::Resolver;
 use sha2::{Digest, Sha256};
 use std::{
     collections::BTreeSet,
+    fs,
+    net::SocketAddr,
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 #[derive(Parser, Debug)]
@@ -24,6 +34,12 @@ struct Args {
 
     #[arg(long, value_delimiter = ' ', num_args = 1..)]
     ports: Vec<u16>,
+
+    #[arg(long)]
+    tsig_key: PathBuf,
+
+    #[arg(long)]
+    rfc2136_nameserver: SocketAddr,
 }
 
 struct Pubkeys([Vec<u8>; 2]);
@@ -61,6 +77,19 @@ pub fn sha256(data: &[u8]) -> Vec<u8> {
     hasher.finalize().to_vec()
 }
 
+fn read_tsig_key(tsig_key: &Path) -> anyhow::Result<tsig::Key> {
+    let content = fs::read_to_string(tsig_key)?;
+    let parts: Vec<_> = content.trim().split(':').collect();
+    let [name, algo, data] = parts.as_slice() else {
+        anyhow::bail!("Invalid key file format");
+    };
+    Ok(tsig::Key::new(
+        name.parse()?,
+        TsigAlgorithm::from_name(algo.parse()?),
+        BASE64.decode(data.as_bytes())?,
+    ))
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     println!("current_key_file: {}!", args.current_key_file.display());
@@ -68,31 +97,47 @@ fn main() -> anyhow::Result<()> {
 
     //let pubkey = load_pubkey(args.current_key_file)?;
     let pubkeys = Pubkeys::load(&args.current_key_file, &args.next_key_file)?;
-    dbg!(pubkeys.tlsa_rdata());
+    //dbg!(pubkeys.tlsa_rdata());
+
+    let tsig_key = read_tsig_key(&args.tsig_key)
+        .with_context(|| format!("Error reading from {}", args.tsig_key.display()))?;
+
+    let resolver = Resolver::from_system_conf()?;
+
+    let client_connection = TcpClientConnection::new(args.rfc2136_nameserver)?;
+    let tsigner = TSigner::new(tsig_key.secret, tsig_key.algorithm, tsig_key.name, 300)?;
+    let sync_client = SyncClient::with_tsigner(client_connection, tsigner);
+    let origin = Name::from_str(&format!("{}.", args.domain_name))?;
 
     //dbg!(&args.ports);
     for port in args.ports.iter() {
         let domain_name = format!("_{}._tcp.{}", port, &args.domain_name);
         println!("{}", domain_name);
 
-        let resolver = Resolver::from_system_conf()?;
         let responses: BTreeSet<_> = match resolver.tlsa_lookup(&domain_name) {
             Ok(x) => x.into_iter().map(RData::TLSA).collect(),
             Err(_) => BTreeSet::new(),
         };
 
-        let diff1: BTreeSet<_> = pubkeys
+        let missing: BTreeSet<_> = pubkeys
             .tlsa_rdata()
             .difference(&responses)
             .cloned()
             .collect();
-        dbg!(&diff1);
 
-        let diff2: BTreeSet<_> = responses
+        for rr in &missing {
+            let mut record = Record::with(Name::from_str(&domain_name)?, RecordType::TLSA, 3600);
+            record.set_data(Some(rr.clone()));
+            sync_client.create(record, origin.clone())?;
+        }
+
+        //dbg!(&missing);
+
+        let excess: BTreeSet<_> = responses
             .difference(&pubkeys.tlsa_rdata())
             .cloned()
             .collect();
-        dbg!(&diff2);
+        //dbg!(&excess);
     }
 
     Ok(())
